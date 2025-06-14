5fa93c8c21b38cd12c97aa940f1b4b4abd4072e9
faith993 <chepkemoitoo3@gmail.com>
2025-06-14 04:58:23 +0300

    Fix ESLint console warnings for production build

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,4 @@
-// .prettierrc
-  {
-    "semi": true,
-    "trailingComma": "es5",
-    "singleQuote": true,
-    "printWidth": 80,
-    "tabWidth": 2,
-    "useTabs": false,
-    "bracketSpacing": true,
-    "arrowParens": "avoid",
-    "endOfLine": "lf"
-  }
-  
+
   // cypress.config.js
   const { defineConfig } = require('cypress');
   
