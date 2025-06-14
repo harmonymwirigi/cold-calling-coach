bf9652906b012f19be058098a52636bb23d8efe9
faith993 <chepkemoitoo3@gmail.com>
2025-06-14 05:30:11 +0300

    Fix ESLint console warnings for production build

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,23 @@
-// .eslintrc.js
 module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
-      node: true
-    },
-    extends: [
-      'eslint:recommended',
-      'plugin:react/recommended',
-      'plugin:react-hooks/recommended'
-    ],
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true
-      },
-      ecmaVersion: 12,
-      sourceType: 'module'
-    },
-    plugins: ['react', 'react-hooks'],
-    rules: {
-      'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
-      'react/react-in-jsx-scope': 'off',
-      'react/prop-types': 'off',
-      'no-unused-vars': 'warn',
-      'react-hooks/exhaustive-deps': 'warn'
-    },
-    settings: {
-      react: {
-        version: 'detect'
-      }
+  extends: [
+    'react-app',
+    'react-app/jest'
+  ],
+  rules: {
+    'react-hooks/exhaustive-deps': 'warn',
+    'no-console': process.env.NODE_ENV === 'production' ? 'off' : 'warn',
+    'no-unused-vars': 'warn'
+  },
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
     }
-  };
-  
-  
+  }
+};
